85ebcf666b0107c60fd2112c996e9313b4f1de20
System Administrator <admin@example.com>
2019-04-15 13:34:36 -0400

    Changes by 'System Administrator' on 'global update set test 3' -- Build #1

---
+++ sn/Global/Script Include/active_true/global.myChangeUtil.js
@@ -0,0 +1,44 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_include
+ * Created On  : 2019-04-15 16:58:51
+ * Created By  : admin
+ * Updated On  : 2019-04-15 17:12:09
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=9204c786db703300bf2c656b4b961981
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof global.module:sys_script_include
+ */
+var myChangeUtil = Class.create();
+myChangeUtil.prototype = /** @lends global.module:sys_script_include.myChangeUtil.prototype */ {
+	/**SNDOC
+	@name methodName
+	@description description
+
+	@param {type} [paramName] - description
+
+	@example
+	example here
+
+	@returns {type} description
+	*/
+    initialize: function() {
+    },
+
+	/**
+	 * Description check for open changes assigned to the same assignment group.
+	 * 
+	 * @param {assignment group reference} assignment_group
+	 * @returns {undefined}
+	 */
+	checkForOpenChangesByGroup: function(assignment_group){
+		
+	},
+	
+    type: 'myChangeUtil'
+};
